6576b2fed3783b6c51bc861c36b284715fa0ac9a
paduszka <paduch.anna@gmail.com>
2017-05-10 09:49:38 +0200

    konfiguracja sass’a

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# portfolio-
-Portfolio website 
+# portfolio
+Portfolio website
--- js/out.js
+++ js/out.js
@@ -68,9 +68,16 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
-
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
